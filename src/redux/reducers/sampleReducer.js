a26c7f898b6cdfd32e3e1093e987e5c50e8f8538
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-14 07:36:57 -0600

    initial commit

---
+++ src/redux/reducers/sampleReducer.js
@@ -0,0 +1,10 @@
+const sampleReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'DISPATCH_TYPE':
+            return 'Hello from sampleReducer';
+        default:
+            return state;
+    }
+}
+
+export default sampleReducer;
